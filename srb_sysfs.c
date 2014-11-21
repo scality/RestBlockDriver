9d39cb6dab77418c1e32c0f9c8fe9b6f227b3035
Nicolas Trangez <ikke@nicolast.be>
2014-11-21 11:08:26 +0100

    scan-build: Value stored to 'tmpend' during its initialization is never read

--- srb_sysfs.c
+++ srb_sysfs.c
@@ -558,7 +558,7 @@ static ssize_t class_srb_addurl_store(struct class *c,
 	ssize_t		ret = 0;
 	char		url[SRB_URL_SIZE+1];
 	const char	*tmp = buf;
-	const char	*tmpend = tmp;
+	const char	*tmpend = NULL;
 	int		errcount = 0;
 
 	while (tmp != NULL) {
