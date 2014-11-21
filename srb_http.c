35fd4a666b4f231853a94a44f8cd42f2434fb234
Nicolas Trangez <ikke@nicolast.be>
2014-11-21 16:06:22 +0100

    Fix HTTP status code
    Typo, '40 ' instead of '406'.
    
    Found by accident through `checkpatch.pl`:
    
    ```
    ERROR: space prohibited before that ':' (ctx:WxW)
    +               case 400: case 401: case 402: case 403: case 404: case 405: case 40 : case 407: case 408: case 409:
    ```

--- srb_http.c
+++ srb_http.c
@@ -88,7 +88,7 @@ int srb_http_get_status(char *buf, int len, enum srb_http_statuscode *code)
 		case 100: case 101:
 		case 200: case 201: case 202: case 203: case 204: case 205: case 206:
 		case 300: case 301: case 302: case 303: case 304: case 305: case 307:
-		case 400: case 401: case 402: case 403: case 404: case 405: case 40 : case 407: case 408: case 409:
+		case 400: case 401: case 402: case 403: case 404: case 405: case 406: case 407: case 408: case 409:
 		case 410: case 411: case 412: case 413: case 414: case 415: case 416: case 417:
 		case 500: case 501: case 502: case 503: case 504: case 505:
 			*code = status;
